c51ebccf38d39314d5cb8813767d63ccbe75fc3c
Abdurahmon Mirmaxsudov <abdurahmonmirmaxsudov2804@gmail.com>
2026-05-28 23:30:51 +0500

    refactor: Enabled caching and async

--- src/main/java/uz/mirmaxsudov/lmsbackend/LmsBackendApplication.java
+++ src/main/java/uz/mirmaxsudov/lmsbackend/LmsBackendApplication.java
@@ -2,7 +2,11 @@ package uz.mirmaxsudov.lmsbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableAsync;
 
+@EnableAsync
+@EnableCaching
 @SpringBootApplication
 public class LmsBackendApplication {
     public static void main(String[] args) {
